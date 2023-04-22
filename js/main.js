d91f7903515daf4129620cea94ad696ae97f9a9e
VarinCode <94356640+VarinCode@users.noreply.github.com>
2023-04-22 03:05:14 +0000

    update project

--- js/main.js
+++ js/main.js
@@ -1,32 +1,34 @@
 const quotation = document.querySelector('#quotation');
 const name = document.querySelector('#author');
-
-(function(){
-	quotation.textContent = 'Press the button to find cool quotes.';
-})();
+const btn = document.querySelector('button');
 
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '033424c2e0msh75b7c318f2705bbp1e00b1jsne4c89a49621a',
-		'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
-	}
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '033424c2e0msh75b7c318f2705bbp1e00b1jsne4c89a49621a',
+        'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+    }
 }
 
-const fetchData = async () => {
-    const data = await fetch('https://quotes15.p.rapidapi.com/quotes/random/', options)
-	await data.json()
-		.then(res => {
-			if(typeof res.content === 'undefined'){
-				quotation.innerHTML = 'Something went wrong, please try again!'
-				name.textContent = ``;	
-			} else {
-				quotation.innerHTML = `" ${res.content} "`;	
-				name.innerHTML = `- ${res.originator.name}`;	
-			}
+const fetchData = async() => {
+    const data = await fetch('https://quotes15.p.rapidapi.com/quotes/random/', options);
+    try {
+        return await data.json();
+    } catch {
+        throw new Error('The api data cannot be retrieved or an error occurred.');
+    }
 
+}
+window.addEventListener('load', () => { quotation.textContent = 'Press the button to find cool quotes.' });
+btn.addEventListener('click', () => {
+    fetchData()
+        .then(res => {
+            if (typeof res.content === 'undefined') {
+                alert(`Something went wrong, please try again!`);
+            } else {
+                quotation.innerHTML = `" ${res.content} "`;
+                name.innerHTML = `- ${res.originator.name}`;
+            }
         })
-		.catch((rej) => console.error(rej));   
-}
-
-window.addEventListener('click' , () => { fetchData() });
+        .catch((rej) => console.error(rej));
+});
